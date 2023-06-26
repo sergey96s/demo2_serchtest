d0f2bee00b1db7572f32ce9ec6ba3c1511e0a94a
Сергей Серебряков <serzh.serebryakov.96@gmail.com>
2023-06-26 23:20:55 +0700

    Checking if the first result is "https://www.selenium.dev/"

--- src/test/java/com/example/demo2_serchtest/MainPageTest.java
+++ src/test/java/com/example/demo2_serchtest/MainPageTest.java
@@ -1,5 +1,6 @@
 package com.example.demo2_serchtest;
 
+import com.beust.jcommander.Strings;
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,8 +10,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainPageTest {
     private WebDriver driver;
@@ -35,14 +40,29 @@ public class MainPageTest {
 
     @Test
     public void search() {
-        String input = "Selenium";
 
+        String input = "Selenium";
         WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
         searchField.sendKeys(input);
         searchField.submit();
-
-        WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
-        assertEquals(input, searchPageField.getAttribute("value"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.and(
+                ExpectedConditions.attributeContains(By.cssSelector("h2 > a[href]"), "href", "selenium"),
+                ExpectedConditions.elementToBeClickable(By.cssSelector("h2 > a[href]"))
+        ));
+        List<WebElement> results = driver.findElements(By.cssSelector("h2 > a[href]"));
+        clickElement(results, 0);
+        ArrayList<String> tabs = new ArrayList<> (driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+        WebDriverWait wait2 = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.not(ExpectedConditions.urlContains("bing.com")));
+        String adress = driver.getCurrentUrl();
+        Assertions.assertEquals("https://www.selenium.dev/", adress, "Не совпадает");
     }
 
+
+    public void clickElement(List<WebElement> results, int num) {
+        results.get(num).click();
+        System.out.println("Нажал на первый элемент");
+    }
 }
